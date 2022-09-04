2f0ed6b720cc91245792ada4fb8ebec874f203cb
Lucas Santiago <llucasanttiago@gmail.com>
2022-09-04 17:13:03 -0300

    Inicio do BACKEND

---
+++ js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12345',
+                nome: 'Silvio',
+                partido: 'AAA',
+                fotos: [
+                    {url: '../images/img1.png', legenda: 'Vereador'}
+             ]   
+            },
+            {
+                numero: '67890',
+                nome: 'Vitor',
+                partido: 'BBB',
+                fotos: [
+                    {url:'../images/img2.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Cleiton',
+                partido: 'CC',
+                vice: 'Janilson',
+                fotos: [
+                    {url: '../images/img3.png', lengenda: 'Prefeito'},
+                    {url: '../images/img3.png', lengenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '34',
+                nome: 'Armando',
+                partido: 'DD',
+                vice: 'Clodoaldo',
+                fotos: [
+                    {url: '../images/img4.png', legenda: 'Prefeito'},
+                    {url: '../images/img4.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
